383a4bb9a37831e04078b9dfdac05514629b0856
wym <pdf.converter@example.com>
2026-03-23 11:46:10 +0800

    fix: add illegalParameters check to prevent JDBC RCE (CVE-2026-32140)

--- core/core-backend/src/main/java/io/dataease/datasource/type/Mongo.java
+++ core/core-backend/src/main/java/io/dataease/datasource/type/Mongo.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import java.net.URLDecoder;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,13 +15,18 @@ import java.util.List;
 public class Mongo extends DatasourceConfiguration {
     private String driver = "com.mysql.cj.jdbc.Driver";
     private String extraParams = "characterEncoding=UTF-8&connectTimeout=5000&useSSL=false&allowPublicKeyRetrieval=true&zeroDateTimeBehavior=convertToNull";
-    private List<String> illegalParameters = Arrays.asList("autoDeserialize", "queryInterceptors", "statementInterceptors", "detectCustomCollations");
+    private List<String> illegalParameters = Arrays.asList("maxAllowedPacket", "autoDeserialize", "queryInterceptors", "statementInterceptors", "detectCustomCollations", "allowloadlocalinfile", "allowUrlInLocalInfile", "allowLoadLocalInfileInPath");
     private List<String> showTableSqls = Arrays.asList("show tables");
 
     public String getJdbc() {
         if (StringUtils.isNoneEmpty(getUrlType()) && !getUrlType().equalsIgnoreCase("hostName")) {
             if (!getJdbcUrl().startsWith("jdbc:mysql")) {
                 DEException.throwException("Illegal jdbcUrl: " + getJdbcUrl());
+            }
+            for (String illegalParameter : illegalParameters) {
+                if (URLDecoder.decode(getJdbcUrl()).toLowerCase().contains(illegalParameter.toLowerCase())) {
+                    DEException.throwException("Illegal parameter: " + illegalParameter);
+                }
             }
             return getJdbcUrl();
         }
